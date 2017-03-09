28385bbce96cf3c800193b0f0e5b74cd16af6b7c
arnaud <songac.arnaud@gmail.com>
2017-03-09 17:26:18 -0500

    Create argument parser

---
+++ ArgsParser.py
@@ -0,0 +1,31 @@
+'''
+Generate Argument Parsers for the server
+
+@author: arno
+'''
+
+import argparse
+
+def generateArgParsers():
+    parser = argparse.ArgumentParser(
+            description="httpfs is a simple file server.")
+    
+    parser.add_argument('-v',
+                              dest="isVerbose",
+                              action="store_const", const=True, default=False,
+                              help="Prints debugging messages.")
+    parser.add_argument('-p',
+                              dest="port",
+                              action="store",
+                              metavar="PORT",
+                              type=int,
+                              default=8080,
+                              help="Specifies the port number that the server will listen and serve at.")
+    
+    parser.add_argument('-d',
+                              dest="homeDir",
+                              action="store",
+                              metavar="PATH-TO-DIR",
+                              default=".",
+                              help="Specifies the directory that the server will use to read/write requested files. Default is the current directory when launching the application.")
+    return parser
---
+++ HttpServer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import socket
+import threading
+import ArgsParser
+
+
+def run_server(host, port):
+    listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        listener.bind((host, port))
+        listener.listen(5)
+        print('Echo server is listening at', port)
+        while True:
+            conn, addr = listener.accept()
+            threading.Thread(target=handle_client, args=(conn, addr)).start()
+    finally:
+        listener.close()
+        
+def handle_client(conn, addr):
+    print('New client from', addr)
+    try:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+    finally:
+        conn.close()
+
+# Usage python echoserver.py [--port port-number]
+parser = ArgsParser.generateArgParsers()
+args = parser.parse_args()
+# run_server('', args.port)
